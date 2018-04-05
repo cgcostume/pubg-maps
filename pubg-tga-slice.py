c63ba749dc9bafdf2fc4a66ccffbcc343a2d6425
Daniel Limberger <cgdaniel@outlook.com>
2018-04-05 21:57:35 +0200

    update maps to 3.7.27

---
+++ pubg-tga-slice.py
@@ -0,0 +1,142 @@
+
+import argparse
+import math
+import numpy
+import os
+import sys
+
+from PIL import Image
+
+# parse arguments
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--umodel_export_path', help = 'umodel export path')
+parser.add_argument('-o', '--output_path', help = 'working directory for extracting and stitching assets', default = '.')
+parser.add_argument('-m', '--map', help = 'map identifier, either Erangel or Miramar', default = 'erangel')
+parser.add_argument('-l', '--lod', help = 'level-of-detail, either 0, 1, or 2', default = '0')
+parser.add_argument('-c', '--compress', help = 'compression level, number between 0 and 10', default = '0')
+
+args = parser.parse_args()
+
+# pakFileNamesByMapNames = {
+#     'erangel' : 'TslGame-WindowsNoEditor_erangel_heightmap.pak', 
+#     'miramar' : 'TslGame-WindowsNoEditor_desert_heightmap.pak' }
+
+tslHeightmapPathsByMap =  {
+	'erangel' : r'Maps\Erangel\Art\Heightmap', 
+	'miramar' : r'Maps\Desert\Art\Heightmap' }
+
+mapIdentifier = args.map.lower()
+if mapIdentifier not in  {'erangel', 'miramar'}:
+    sys.exit('unknown map identifier \'' + mapIdentifier + '\'')
+
+
+assert os.path.isdir(args.umodel_export_path)
+umodel_heightmap_path = os.path.abspath(os.path.join(args.umodel_export_path, tslHeightmapPathsByMap[mapIdentifier]))
+assert os.path.isdir(umodel_heightmap_path)
+
+output_path = args.output_path
+if not os.path.isdir(output_path):
+    os.makedirs(output_path)
+assert os.path.isdir(output_path)
+
+
+normal_semantic = 'normal_rg8'
+height_semantic = 'height_l16'
+
+lod = int(args.lod)
+compress = int(args.compress)
+
+# slicing data
+
+tile_width = 512 # { 0: 512, 1: 256, 2: 128 }[lod]
+tile_height = 512 # { 0: 512, 1: 256, 2: 128 }[lod]
+# tile_channels = 4
+# tile_offset = int({ 0: 0, 1: 512 * 512 * 4 * (1.0), 2: 512 * 512 * 4 * (1.0 + 0.25)}[lod])
+
+tile_size = int(tile_width * tile_height)
+# tile_size_with_mipmaps = int(tile_size * tile_channels * (1.00 + 0.25 + 0.0625))
+
+
+def extract_tiles(asset_path, offsets, height_target, normal_target):
+
+	sequence_index = 0
+	for i, indices in enumerate([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]]):
+		for tile_index in indices:
+
+			tile_path = asset_path % (offsets[0], offsets[1], i, tile_index)
+			# print (tile_path)
+	
+			tile = Image.open(tile_path)
+			tile_r, tile_g, tile_b, tile_a = tile.split()
+
+			# extract normal	
+			
+			channel_r = numpy.asarray(tile_b).flatten()
+			channel_g = numpy.asarray(tile_a).flatten()
+
+			channel_b = numpy.resize(numpy.uint8(), tile_size)
+			channel_b.fill(255)
+
+			rgb = numpy.dstack((channel_r, channel_g, channel_b)).flatten()
+
+			# # extract elevation
+
+			# height_uint16 = numpy.fromstring(tile[1:] + b'\x00', numpy.uint16)
+			# channel_l = height_uint16[0::2]
+		
+			# refine stitching sequence
+
+			x = offsets[0] * 4 + int(i % 2) * 2 + int(tile_index % 2)
+			y = offsets[1] * 4 + int(i / 2) * 2 + int(tile_index / 2)
+
+			ordered_index = y * 16 + x
+
+			# paste data
+
+			target_x = (ordered_index % 16) * tile_width
+			target_y = math.floor(ordered_index / 16) * tile_height
+
+			# write normal tile
+			
+			normal_tile = Image.frombytes('RGB', (tile_width, tile_height), rgb)
+			normal_target.paste(normal_tile, (target_x, target_y))
+
+			# write height tile
+
+			channel_l = numpy.left_shift(numpy.asarray(tile_r, numpy.uint16()).flatten(), 8)
+			channel_l = channel_l + numpy.asarray(tile_g).flatten()
+		
+			height_tile = Image.frombytes('I;16', (tile_width, tile_height), numpy.asarray(channel_l, order = 'C'))
+			height_target.paste(height_tile, (target_x, target_y))
+
+			# display progress
+
+			progress = (offsets[0] * 4 + offsets[1]) * 16 + sequence_index + 1
+			print ('processing', str(progress).rjust(3, '0'), 'of 256', flush = True, end = ('\r' if progress < 256 else '\n'))
+
+			sequence_index += 1
+
+	# 	ubulk.close()
+
+
+
+print ('extracting 256 tiles (normal and height data) ...')
+
+normal_composite = Image.new("RGB", (tile_width * 16, tile_height * 16))
+height_composite = Image.new("I", (tile_width * 16, tile_height * 16))
+
+
+for indices in [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 2), (1, 3), (2, 0), (2, 1), (2, 2), (2, 3), (3, 0), (3, 1), (3, 2), (3, 3)]:
+ 	# example '.\UmodelExport\Maps\Erangel\Art\Heightmap\Heightmap_x0_y0_00_sharedAssets\Texture2D_0.tga'
+	asset_path = os.path.join(umodel_heightmap_path, 'Heightmap_x%d_y%d_0%d_sharedAssets\Texture2D_%d.tga')
+	extract_tiles(asset_path, (indices[0], indices[1]), height_composite, normal_composite)
+
+
+normal_stitched_path = os.path.join(output_path, 'pubg_' + mapIdentifier + '_' + normal_semantic + '_lod' + str(lod) + '.png')
+print (normal_stitched_path, 'saving', ['8k', '4k', '2k'][lod], 'normal map ... hang in there')
+normal_composite.save(normal_stitched_path, 'PNG', compress_level = min(9, compress), optimize = compress == 10)
+
+height_stitched_path = os.path.join(output_path, 'pubg_' + mapIdentifier + '_' + height_semantic + '_lod' + str(lod) + '.png')
+print (normal_stitched_path, 'saving', ['8k', '4k', '2k'][lod], 'height map ... hang in there')
+height_composite.save(height_stitched_path, 'PNG', compress_level = min(9, compress), optimize = compress == 10)
