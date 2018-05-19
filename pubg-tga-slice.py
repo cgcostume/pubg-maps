75e0a88d9bef1e5569d0121bcc5a614dfd10a2dd
Daniel Limberger <cgdaniel@outlook.com>
2018-05-19 09:21:18 +0200

    prepare tga slice for smaller maps (savage)

--- pubg-tga-slice.py
+++ pubg-tga-slice.py
@@ -12,7 +12,7 @@ from PIL import Image
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--umodel_export_path', help = 'umodel export path')
 parser.add_argument('-o', '--output_path', help = 'working directory for extracting and stitching assets', default = '.')
-parser.add_argument('-m', '--map', help = 'map identifier, either Erangel or Miramar', default = 'erangel')
+parser.add_argument('-m', '--map', help = 'map identifier, either erangel, miramar, or savage', default = 'erangel')
 # parser.add_argument('-l', '--lod', help = 'level-of-detail, either 0, 1, or 2', default = '0')
 parser.add_argument('-c', '--compress', help = 'compression level, number between 0 and 10', default = '0')
 
@@ -24,11 +24,16 @@ args = parser.parse_args()
 
 tslHeightmapPathsByMap =  {
 	'erangel' : r'Maps\Erangel\Art\Heightmap', 
-	'miramar' : r'Maps\Desert\Art\Heightmap' }
+	'miramar' : r'Maps\Desert\Art\Heightmap',
+	'sahok' : r'Maps\Savage\Art\Heightmap', }
+
 
 mapIdentifier = args.map.lower()
-if mapIdentifier not in  {'erangel', 'miramar'}:
+if mapIdentifier not in  {'erangel', 'miramar', 'sahok' }:
     sys.exit('unknown map identifier \'' + mapIdentifier + '\'')
+
+smallMap = mapIdentifier == 'sahok'
+numTiles = 64 if smallMap else 256
 
 
 assert os.path.isdir(args.umodel_export_path)
@@ -54,18 +59,26 @@ tile_height = { 0: 512, 1: 256, 2: 128 }[lod]
 # tile_channels = 4
 # tile_offset = int({ 0: 0, 1: 512 * 512 * 4 * (1.0), 2: 512 * 512 * 4 * (1.0 + 0.25)}[lod])
 
+tile_scale = 8 if smallMap else 16;
 tile_size = int(tile_width * tile_height)
 # tile_size_with_mipmaps = int(tile_size * tile_channels * (1.00 + 0.25 + 0.0625))
 
 
 def extract_tiles(asset_path, offsets, height_target, normal_target):
 
+	tile_indices = [[0, 1, 2, 3]] if smallMap else [[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]]
+	num_indices = len(numpy.array(tile_indices).flatten())
+
+	offset_scale = 2 if smallMap else 4
+
 	sequence_index = 0
-	for i, indices in enumerate([[0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3], [0, 1, 2, 3]]):
+	for i, indices in enumerate(tile_indices):
 		for tile_index in indices:
 
-			tile_path = asset_path % (offsets[0], offsets[1], i, tile_index)
-			# print (tile_path)
+			if smallMap:
+				tile_path = asset_path % (offsets[0], offsets[1], tile_index)
+			else:
+				tile_path = asset_path % (offsets[0], offsets[1], i, tile_index)
 	
 			tile = Image.open(tile_path)
 			tile_r, tile_g, tile_b, tile_a = tile.split()
@@ -87,8 +100,8 @@ def extract_tiles(asset_path, offsets, height_target, normal_target):
 		
 			# refine stitching sequence
 
-			x = offsets[0] * 4 + int(i % 2) * 2 + int(tile_index % 2)
-			y = offsets[1] * 4 + int(i / 2) * 2 + int(tile_index / 2)
+			x = offsets[0] * offset_scale + int(i % 2) * 2 + int(tile_index % 2)
+			y = offsets[1] * offset_scale + int(i / 2) * 2 + int(tile_index / 2)
 
 			ordered_index = y * 16 + x
 
@@ -109,8 +122,10 @@ def extract_tiles(asset_path, offsets, height_target, normal_target):
 
 			# display progress
 
-			progress = (offsets[0] * 4 + offsets[1]) * 16 + sequence_index + 1
-			print ('processing', str(progress).rjust(3, '0'), 'of 256', flush = True, end = ('\r' if progress < 256 else '\n'))
+			progress = (offsets[0] * 4 + offsets[1]) * num_indices + sequence_index + 1
+			
+			print ('processing', str(progress).rjust(2 if smallMap else 3, '0'), 'of', numTiles, 
+				flush = True, end = ('\r' if progress < numTiles else '\n'))
 
 			sequence_index += 1
 
@@ -118,17 +133,25 @@ def extract_tiles(asset_path, offsets, height_target, normal_target):
 
 
 
-print ('extracting 256 tiles (normal and height data) ...')
+print ('extracting', numTiles, 'tiles (normal and height data) ...')
 
-normal_composite = Image.new("RGB", (tile_width * 16, tile_height * 16))
-height_composite = Image.new("I", (tile_width * 16, tile_height * 16))
+normal_composite = Image.new("RGB", (tile_width * tile_scale, tile_height * tile_scale))
+height_composite = Image.new("I", (tile_width * tile_scale, tile_height * tile_scale))
 
 
 for indices in [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), (1, 1), (1, 2), (1, 3), (2, 0), (2, 1), (2, 2), (2, 3), (3, 0), (3, 1), (3, 2), (3, 3)]:
  	# example '.\UmodelExport\Maps\Erangel\Art\Heightmap\Heightmap_x0_y0_00_sharedAssets\Texture2D_0.tga'
-	asset_path = os.path.join(umodel_heightmap_path, 'Heightmap_x%d_y%d_0%d_sharedAssets\Texture2D_%d.tga')
+	
+	path = 'Heightmap_x%d_y%d_'
+	if not smallMap:
+		path = path + '0%d_'
+	path = path + 'sharedAssets\Texture2D_%d.tga'
+
+	asset_path = os.path.join(umodel_heightmap_path, path)
 	extract_tiles(asset_path, (indices[0], indices[1]), height_composite, normal_composite)
 
+if smallMap:
+	lod = lod + 1
 
 normal_stitched_path = os.path.join(output_path, 'pubg_' + mapIdentifier + '_' + normal_semantic + '_lod' + str(lod) + '.png')
 print (normal_stitched_path, 'saving', ['8k', '4k', '2k'][lod], 'normal map ... hang in there')
